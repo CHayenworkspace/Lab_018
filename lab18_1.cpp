c121a9e8b30c71074fb0cfe4c7ed5de4207e44b0
chayen030 <chayenlol268@gmail.com>
2025-02-07 15:07:08 +0700

    101

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -7,6 +7,16 @@ struct Rect{
 
 double overlap(){
 
+ 	int tx[] = {T.x,T.x+T.w,T.x,T.x+T.w};
+    int Rx[] = {R.x,R.x+R.w,R.x,R.x+R.w};
+    int ty[] = {T.y,T.y-T.h,T.y,T.y-T.w};
+    int Ry[] = {R.y,R.y-R.h,R.y,R.y-R.w};
 
+}
 
+int main{
+
+	Rect R1 = {1,1,5,5};
+	Rect R2 = {2,2,5,5};	
+	cout << overlap(R1,R2);	
 }
