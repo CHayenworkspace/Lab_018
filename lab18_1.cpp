51c131804b7b8bfd4465d3a30474abe868a810e3
chayen030 <chayenlol268@gmail.com>
2025-02-07 15:25:45 +0700

    102

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,18 +5,12 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R,Rect T){
 
- 	int tx[] = {T.x,T.x+T.w,T.x,T.x+T.w};
-    int Rx[] = {R.x,R.x+R.w,R.x,R.x+R.w};
-    int ty[] = {T.y,T.y-T.h,T.y,T.y-T.w};
-    int Ry[] = {R.y,R.y-R.h,R.y,R.y-R.w};
-
-}
-
-int main{
-
-	Rect R1 = {1,1,5,5};
-	Rect R2 = {2,2,5,5};	
-	cout << overlap(R1,R2);	
+ 	double tsx[] = {T.x+T.w,T.x+T.h,T.x+T.w,T.x+T.h};
+    double rsx[] = {R.x,R.x+R.w,R.x,R.x+R.w};
+    double tsy[] = {T.y-T.w,T.y-T.h,T.y-T.w,T.y-T.h};
+    double rsy[] = {R.y-R.w,R.y-R.h,R.y-R.w,R.y-R.w};
+    if (tsy[1]/tsy[1]==-1||rsy[1]/rsy[1]==-1){
+    int x = tsy[1]-rsy[1] ;}
 }
